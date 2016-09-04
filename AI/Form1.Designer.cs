f93e755bd4230fb2277c29e4eefe10e29b734e7e
nuhin <nuhin13@gmail.com>
2016-09-04 21:59:29 +0600

    commit for tic tac toe

---
+++ AI/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace AI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Board00 = new System.Windows.Forms.Button();
+            this.Board10 = new System.Windows.Forms.Button();
+            this.Board20 = new System.Windows.Forms.Button();
+            this.Board21 = new System.Windows.Forms.Button();
+            this.Board11 = new System.Windows.Forms.Button();
+            this.Board01 = new System.Windows.Forms.Button();
+            this.Board22 = new System.Windows.Forms.Button();
+            this.Board12 = new System.Windows.Forms.Button();
+            this.Board02 = new System.Windows.Forms.Button();
+            this.via_players = new System.Windows.Forms.Button();
+            this.via_computer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Board00
+            // 
+            this.Board00.Location = new System.Drawing.Point(46, 155);
+            this.Board00.Name = "Board00";
+            this.Board00.Size = new System.Drawing.Size(89, 44);
+            this.Board00.TabIndex = 0;
+            this.Board00.UseVisualStyleBackColor = true;
+            this.Board00.Click += new System.EventHandler(this.ClickingButton);
+            // 
+            // Board10
+            // 
+            this.Board10.Location = new System.Drawing.Point(46, 205);
+            this.Board10.Name = "Board10";
+            this.Board10.Size = new System.Drawing.Size(89, 44);
+            this.Board10.TabIndex = 1;
+            this.Board10.UseVisualStyleBackColor = true;
+            this.Board10.Click += new System.EventHandler(this.ClickingButton);
+            // 
+            // Board20
+            // 
+            this.Board20.Location = new System.Drawing.Point(46, 255);
+            this.Board20.Name = "Board20";
+            this.Board20.Size = new System.Drawing.Size(89, 44);
+            this.Board20.TabIndex = 2;
+            this.Board20.UseVisualStyleBackColor = true;
+            this.Board20.Click += new System.EventHandler(this.ClickingButton);
+            // 
+            // Board21
+            // 
+            this.Board21.Location = new System.Drawing.Point(156, 255);
+            this.Board21.Name = "Board21";
+            this.Board21.Size = new System.Drawing.Size(89, 44);
+            this.Board21.TabIndex = 5;
+            this.Board21.UseVisualStyleBackColor = true;
+            this.Board21.Click += new System.EventHandler(this.ClickingButton);
+            // 
+            // Board11
+            // 
+            this.Board11.Location = new System.Drawing.Point(156, 205);
+            this.Board11.Name = "Board11";
+            this.Board11.Size = new System.Drawing.Size(89, 44);
+            this.Board11.TabIndex = 4;
+            this.Board11.UseVisualStyleBackColor = true;
+            this.Board11.Click += new System.EventHandler(this.ClickingButton);
+            // 
+            // Board01
+            // 
+            this.Board01.Location = new System.Drawing.Point(156, 155);
+            this.Board01.Name = "Board01";
+            this.Board01.Size = new System.Drawing.Size(89, 44);
+            this.Board01.TabIndex = 3;
+            this.Board01.UseVisualStyleBackColor = true;
+            this.Board01.Click += new System.EventHandler(this.ClickingButton);
+            // 
+            // Board22
+            // 
+            this.Board22.Location = new System.Drawing.Point(265, 255);
+            this.Board22.Name = "Board22";
+            this.Board22.Size = new System.Drawing.Size(89, 44);
+            this.Board22.TabIndex = 8;
+            this.Board22.UseVisualStyleBackColor = true;
+            this.Board22.Click += new System.EventHandler(this.ClickingButton);
+            // 
+            // Board12
+            // 
+            this.Board12.Location = new System.Drawing.Point(265, 205);
+            this.Board12.Name = "Board12";
+            this.Board12.Size = new System.Drawing.Size(89, 44);
+            this.Board12.TabIndex = 7;
+            this.Board12.UseVisualStyleBackColor = true;
+            this.Board12.Click += new System.EventHandler(this.ClickingButton);
+            // 
+            // Board02
+            // 
+            this.Board02.Location = new System.Drawing.Point(265, 155);
+            this.Board02.Name = "Board02";
+            this.Board02.Size = new System.Drawing.Size(89, 44);
+            this.Board02.TabIndex = 6;
+            this.Board02.UseVisualStyleBackColor = true;
+            this.Board02.Click += new System.EventHandler(this.ClickingButton);
+            // 
+            // via_players
+            // 
+            this.via_players.Location = new System.Drawing.Point(46, 43);
+            this.via_players.Name = "via_players";
+            this.via_players.Size = new System.Drawing.Size(308, 38);
+            this.via_players.TabIndex = 9;
+            this.via_players.Text = "2 Players";
+            this.via_players.UseVisualStyleBackColor = true;
+            this.via_players.Click += new System.EventHandler(this.via_players_Click);
+            // 
+            // via_computer
+            // 
+            this.via_computer.Location = new System.Drawing.Point(46, 96);
+            this.via_computer.Name = "via_computer";
+            this.via_computer.Size = new System.Drawing.Size(308, 36);
+            this.via_computer.TabIndex = 10;
+            this.via_computer.Text = "Player vs Computer";
+            this.via_computer.UseVisualStyleBackColor = true;
+            this.via_computer.Click += new System.EventHandler(this.via_computer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 340);
+            this.Controls.Add(this.via_computer);
+            this.Controls.Add(this.via_players);
+            this.Controls.Add(this.Board22);
+            this.Controls.Add(this.Board12);
+            this.Controls.Add(this.Board02);
+            this.Controls.Add(this.Board21);
+            this.Controls.Add(this.Board11);
+            this.Controls.Add(this.Board01);
+            this.Controls.Add(this.Board20);
+            this.Controls.Add(this.Board10);
+            this.Controls.Add(this.Board00);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Board00;
+        private System.Windows.Forms.Button Board10;
+        private System.Windows.Forms.Button Board20;
+        private System.Windows.Forms.Button Board21;
+        private System.Windows.Forms.Button Board11;
+        private System.Windows.Forms.Button Board01;
+        private System.Windows.Forms.Button Board22;
+        private System.Windows.Forms.Button Board12;
+        private System.Windows.Forms.Button Board02;
+        private System.Windows.Forms.Button via_players;
+        private System.Windows.Forms.Button via_computer;
+    }
+}
+
---
+++ AI/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int players = 2;
+        int moves = 0;
+       
+
+        bool player_vs_computer = true;
+       
+
+        private void ClickingButton(object sender, EventArgs e)
+        {
+            Button tic_tac = (Button)sender;
+
+            if (tic_tac.Text == "")
+            {
+                if(flag_forplayers == true)
+                {
+                    if (players % 2 == 0)
+                    {
+                        tic_tac.Text = "X";
+                        players++;
+                        moves++;
+                    }
+
+                    else
+                    {
+                        tic_tac.Text = "O";
+                        players++;
+                        moves++;
+                       
+                    }
+                }
+                else if (flag_for_computer == true)
+                {
+                    if (player_vs_computer)
+                    {
+                        tic_tac.Text = "X";
+                    }
+                    else
+                    {
+                        tic_tac.Text = "O";
+                    }
+
+                    player_vs_computer = !player_vs_computer;                    
+                    moves++;
+
+                    if (!player_vs_computer)
+                    {
+                        computer_make_move();
+                    }
+                }
+
+
+                if (Winner() == true) {
+
+                    if (tic_tac.Text == "X")
+                    {
+                        MessageBox.Show("Player 1 win", "Result");
+                        newgame();
+                    }
+                    else {
+                        if (flag_forplayers == true)
+                        {
+                            MessageBox.Show("Player 2 win", "Result");
+                        }
+                        else if (flag_for_computer == true) {
+                            MessageBox.Show("Computer win", "Result");
+                        }
+                        newgame();
+                    }                
+                }
+                if (drawgame() == true) {
+                    MessageBox.Show("Draw", "Result");
+                    newgame();
+                }
+                
+                
+            }
+        }
+
+
+
+        public bool Winner(){
+        
+            if( (Board00.Text == Board01.Text) && ( Board00.Text == Board02.Text) && Board00.Text != "" ){
+                    return true;
+               }
+            else if ((Board10.Text == Board11.Text) && (Board10.Text == Board12.Text) && Board10.Text != "")
+            {
+                return true;
+            }
+            else if ((Board20.Text == Board21.Text) && (Board20.Text == Board22.Text) && Board20.Text != "")
+            {
+                return true;
+            }
+
+
+            else if ((Board00.Text == Board10.Text) && (Board00.Text == Board20.Text) && Board00.Text != "")
+            {
+                return true;
+            }
+            else if ((Board01.Text == Board11.Text) && (Board01.Text == Board21.Text) && Board01.Text != "")
+            {
+                return true;
+            }
+            else if ((Board02.Text == Board12.Text) && (Board02.Text == Board22.Text) && Board02.Text != "")
+            {
+                return true;
+            }
+
+
+            else if ((Board00.Text == Board11.Text) && (Board00.Text == Board22.Text) && Board00.Text != "")
+            {
+                return true;
+            }
+            else if ((Board02.Text == Board11.Text) && (Board02.Text == Board20.Text) && Board02.Text != "")
+            {
+                return true;
+            }
+
+            else {
+                return false;
+            }
+        }
+
+        public bool drawgame() {
+
+            if (moves == 9)
+                {
+                    return true;
+                }else{
+                    return false; 
+                }
+       }
+
+
+        private void computer_make_move()
+        {
+
+            Button computer_move = null;
+
+            computer_move = for_win_or_draw("O");
+
+            if (computer_move == null)
+            {
+                computer_move = for_win_or_draw("X");
+
+                if (computer_move == null)
+                {
+                    computer_move = for_corner();
+
+                }
+            }
+
+           // if (moves < 9)
+           // {
+                computer_move.PerformClick();
+           // }
+        }
+
+
+        private Button for_corner()
+        {
+            if (Board00.Text == "O")
+            {
+                if (Board02.Text == "")
+                    return Board02;
+                if (Board22.Text == "")
+                    return Board22;
+                if (Board20.Text == "")
+                    return Board20;
+            }
+
+            if (Board02.Text == "O")
+            {
+                if (Board00.Text == "")
+                    return Board00;
+                if (Board22.Text == "")
+                    return Board22;
+                if (Board20.Text == "")
+                    return Board20;
+            }
+
+            if (Board22.Text == "O")
+            {
+                if (Board00.Text == "")
+                    return Board02;
+                if (Board02.Text == "")
+                    return Board02;
+                if (Board20.Text == "")
+                    return Board20;
+            }
+
+            if (Board20.Text == "O")
+            {
+                if (Board00.Text == "")
+                    return Board02;
+                if (Board02.Text == "")
+                    return Board02;
+                if (Board22.Text == "")
+                    return Board22;
+            }
+
+            if (Board00.Text == "")
+                return Board00;
+            if (Board02.Text == "")
+                return Board02;
+            if (Board20.Text == "")
+                return Board20;
+            if (Board22.Text == "")
+                return Board22;
+
+            return null;
+        }
+
+        private Button for_win_or_draw(string symbol)
+        {
+
+            if ((Board00.Text == symbol) && (Board01.Text == symbol) && (Board02.Text == ""))
+                return Board02;
+            if ((Board01.Text == symbol) && (Board02.Text == symbol) && (Board00.Text == ""))
+                return Board00;
+            if ((Board00.Text == symbol) && (Board02.Text == symbol) && (Board01.Text == ""))
+                return Board01;
+
+            if ((Board10.Text == symbol) && (Board11.Text == symbol) && (Board12.Text == ""))
+                return Board12;
+            if ((Board11.Text == symbol) && (Board12.Text == symbol) && (Board10.Text == ""))
+                return Board10;
+            if ((Board10.Text == symbol) && (Board12.Text == symbol) && (Board11.Text == ""))
+                return Board11;
+
+            if ((Board20.Text == symbol) && (Board21.Text == symbol) && (Board22.Text == ""))
+                return Board22;
+            if ((Board21.Text == symbol) && (Board22.Text == symbol) && (Board20.Text == ""))
+                return Board20;
+            if ((Board20.Text == symbol) && (Board22.Text == symbol) && (Board21.Text == ""))
+                return Board21;
+
+
+
+            if ((Board00.Text == symbol) && (Board10.Text == symbol) && (Board20.Text == ""))
+                return Board20;
+            if ((Board10.Text == symbol) && (Board20.Text == symbol) && (Board00.Text == ""))
+                return Board00;
+            if ((Board00.Text == symbol) && (Board20.Text == symbol) && (Board10.Text == ""))
+                return Board10;
+
+            if ((Board01.Text == symbol) && (Board11.Text == symbol) && (Board21.Text == ""))
+                return Board21;
+            if ((Board11.Text == symbol) && (Board21.Text == symbol) && (Board01.Text == ""))
+                return Board01;
+            if ((Board01.Text == symbol) && (Board21.Text == symbol) && (Board11.Text == ""))
+                return Board11;
+
+            if ((Board02.Text == symbol) && (Board12.Text == symbol) && (Board22.Text == ""))
+                return Board22;
+            if ((Board12.Text == symbol) && (Board22.Text == symbol) && (Board02.Text == ""))
+                return Board02;
+            if ((Board02.Text == symbol) && (Board22.Text == symbol) && (Board12.Text == ""))
+                return Board12;
+
+
+
+            if ((Board00.Text == symbol) && (Board11.Text == symbol) && (Board22.Text == ""))
+                return Board22;
+            if ((Board11.Text == symbol) && (Board22.Text == symbol) && (Board00.Text == ""))
+                return Board00;
+            if ((Board00.Text == symbol) && (Board22.Text == symbol) && (Board11.Text == ""))
+                return Board11;
+
+            if ((Board02.Text == symbol) && (Board11.Text == symbol) && (Board20.Text == ""))
+                return Board20;
+            if ((Board11.Text == symbol) && (Board20.Text == symbol) && (Board02.Text == ""))
+                return Board02;
+            if ((Board02.Text == symbol) && (Board20.Text == symbol) && (Board11.Text == ""))
+                return Board11;
+
+            return null;
+        }
+
+        public void newgame(){
+            players=2;
+            moves=0;
+            Board00.Text = Board01.Text = Board02.Text = Board10.Text = Board11.Text = Board12.Text = Board20.Text = Board21.Text = Board22.Text = "";
+        }
+
+        bool flag_forplayers,flag_for_computer;
+        
+        private void via_players_Click(object sender, EventArgs e)
+        {
+            flag_forplayers = true;
+            flag_for_computer = false;
+        }
+
+        private void via_computer_Click(object sender, EventArgs e)
+        {
+            flag_for_computer = true;
+            flag_forplayers = false;
+        }
+    }
+}
